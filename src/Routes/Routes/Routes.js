23509c46a4232b0eda661aa8ddf721265121099a
Md.Al kafee <sojeebahmed.sa@gmail.com>
2022-10-24 06:44:42 +0600

    auth and many things

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,24 +2,37 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Category from "../../Pages/Category/Category/Category";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Login/Register/Register";
 import News from "../../Pages/News/News/News";
 
 export const routes = createBrowserRouter([
     {
-        path:'/',
+        path: '/',
         element: <Main></Main>,
-        children:[
+        children: [
             {
-                path:'/',
-                element: <Home></Home>
+                path: '/',
+                element: <Home></Home>,
+                loader: () => fetch('http://localhost:5000/news')
             },
             {
-                path:'/category/:id',
-                element:<Category></Category>
+                path: '/category/:id',
+                element: <Category></Category>,
+            loader:({params}) => fetch(`http://localhost:5000/category/${params.id}`)
             },
             {
-                path:'/news/:id',
-                element: <News></News>
+                path: '/news/:id',
+                element: <News></News>,
+                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+            },
+            {
+                path:'/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     }
